5b2e713ec06dbec97f2ea7fdc95691d9923d390b
boqiu <82121246@qq.com>
2023-06-28 10:39:24 +0800

    remove useless interface

--- contracts/LedgerInfo.sol
+++ contracts/LedgerInfo.sol
@@ -2,21 +2,35 @@
 
 pragma solidity ^0.8.4;
 
-import "./interface/ILedgerInfo.sol";
 import "./lib/LedgerInfoLib.sol";
 import "./lib/BLS.sol";
 
-contract LedgerInfo is ILedgerInfo {
+contract LedgerInfo {
 
-    function bcsEncode(LedgerInfoLib.LedgerInfoWithSignatures memory ledgerInfo) external pure override returns (bytes memory) {
+    /**
+     * @dev BCS encode the specified `ledgerInfo`.
+     */
+    function bcsEncode(LedgerInfoLib.LedgerInfoWithSignatures memory ledgerInfo) public pure returns (bytes memory) {
         return LedgerInfoLib.bcsEncodeLedgerInfo(ledgerInfo);
     }
 
-    function batchVerifyBLS(bytes[] memory signatures, bytes memory message, bytes[] memory publicKeys) external view override returns (bool) {
+    /**
+     * @dev Verify BLS signatures in batch.
+     * @param signatures uncompressed BLS signatures (each in 192 bytes).
+     * @param message message to verify.
+     * @param publicKeys uncompressed BLS public keys (each in 96 bytes).
+     */
+    function batchVerifyBLS(bytes[] memory signatures, bytes memory message, bytes[] memory publicKeys) public view returns (bool) {
         return BLS.batchVerify(signatures, message, publicKeys);
     }
 
-    function verifyBLS(bytes memory signature, bytes memory message, bytes memory publicKey) external view override returns (bool) {
+    /**
+     * @dev Verifies BLS signature.
+     * @param signature uncompressed BLS signature in 192 bytes.
+     * @param message message to verify.
+     * @param publicKey uncompressed BLS public key in 96 bytes.
+     */
+    function verifyBLS(bytes memory signature, bytes memory message, bytes memory publicKey) public view returns (bool) {
         return BLS.verify(signature, message, publicKey);
     }
 
